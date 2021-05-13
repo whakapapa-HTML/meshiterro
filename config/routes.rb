34fd75d2b82aafb099268255a01ebf683ae27f96
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-05-13 10:50:47 +0000

    [Finish] 3-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
-  devise_for :users
-  resources :post_images,only: [:new,:create,:index,:show,:destroy]
+ devise_for :users 
+ root to: 'homes#top'
+  
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    
+    resources :post_comments, only: [:create, :destroy]
+  end
 end
